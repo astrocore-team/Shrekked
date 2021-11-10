ac800f9dd6047c48c115089e769c49582e2702fb
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-10 13:21:49 +0000

    Update shrek.py

--- shrek.py
+++ shrek.py
@@ -1,15 +1,11 @@
-from __future__ import print_function
-from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
-
-
-def main():
-    sessions = AudioUtilities.GetAllSessions()
-    for session in sessions:
-        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-        if session.Process and session.Process.name() == "vlc.exe":
-            print("volume.GetMasterVolume(): %s" % volume.GetMasterVolume())
-            volume.SetMasterVolume(0.6, None)
-
-
-if __name__ == "__main__":
-    main()
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+ 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+   IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+ 
+# Control volume
+volume.SetMasterVolumeLevel(-0.0, None) #max
