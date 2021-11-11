4a502291156b872e548a1f3b850a2db686b0700f
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-10 17:26:39 -0800

    Update shrek.py

--- shrek.py
+++ shrek.py
@@ -4,43 +4,16 @@ import imageio
 from PIL import Image, ImageTk
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import math
-import os
-import sys
-import Tkinter as tkinter
-import gobject
-import gst
+from tkinter import *
+from tkvideo import tkvideo
 
-video_name = "C:\Users\jake2\Documents\GitHub\Shrekked\MLG SHREK COMPILATION!.MKV"
+root = Tk()
+my_label = Label(root)
+my_label.pack()
+player = tkvideo("C:\Users\jake2\Documents\GitHub\Shrekked\MLG SHREK COMPILATION!", my_label, loop = 1, size = (1280,720))
+player.play()
 
-def on_sync_message(bus, message, window_id):
-        if not message.structure is None:
-            if message.structure.get_name() == 'prepare-xwindow-id':
-                image_sink = message.src
-                image_sink.set_property('force-aspect-ratio', True)
-                image_sink.set_xwindow_id(window_id)
-
-gobject.threads_init()
-
-window = tkinter.Tk()
-window.geometry('500x400')
-
-video = tkinter.Frame(window, bg='#000000')
-video.pack(side=tkinter.BOTTOM,anchor=tkinter.S,expand=tkinter.YES,fill=tkinter.BOTH)
-
-window_id = video.winfo_id()
-
-player = gst.element_factory_make('playbin2', 'player')
-player.set_property('video-sink', None)
-player.set_property('uri', 'C:\Users\jake2\Documents\GitHub\Shrekked\MLG SHREK COMPILATION!.MKV' % (os.path.abspath(sys.argv[1])))
-player.set_state(gst.STATE_PLAYING)
-
-bus = player.get_bus()
-bus.add_signal_watch()
-bus.enable_sync_message_emission()
-bus.connect('sync-message::element', on_sync_message, window_id)
-
-window.mainloop()
+root.mainloop()
 
 # Get default audio device using PyCAW
 devices = AudioUtilities.GetSpeakers()
