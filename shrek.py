3678101baf6234f1c32d6ee9751c720f24b50201
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-10 16:56:02 -0800

    Update shrek.py

--- shrek.py
+++ shrek.py
@@ -1,11 +1,13 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
- 
+import math
+# Get default audio device using PyCAW
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
-   IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
- 
-# Control volume
-volume.SetMasterVolumeLevel(-0.0, None) #max
+# Get current volume 
+currentVolumeDb = volume.GetMasterVolumeLevel()
+volume.SetMasterVolumeLevel(currentVolumeDb - 6.0, None)
+# NOTE: -6.0 dB = half volume !
