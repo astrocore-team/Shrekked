c788daca6d6c2c69d75bdf2b31f0fa5184c3842e
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-15 22:39:56 +0000

    fef

--- shrek.py
+++ shrek.py
@@ -1,7 +1,5 @@
-
 from __future__ import print_function
 from playsound import playsound
-import volume
 from ctypes import cast, POINTER
 import time, os, ctypes, tkinter as tk, random, winsound
 from threading import Thread
@@ -16,6 +14,12 @@ from pydub import AudioSegment
 from pydub import AudioSegment
 from pydub.playback import play
 import subprocess
+from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+from pynput.keyboard import Key,Controller
+keyboard = Controller()
+import time
+import sys
+import subprocess
 
 
 n = -1
@@ -29,10 +33,12 @@ my_label.pack()
 player = tkvideo("C:/Users/Jake Gorham/Documents/GitHub/Shrekked/MLG SHREK COMPILATION!.mp4", my_label, loop = 1, size = (1280,720))
 player.play()
 
-os.system('volume.py')
+for i in range(1000):
+    keyboard.press(Key.media_volume_up)
+    keyboard.release(Key.media_volume_up)
+    time.sleep(0.1)
 # for playing note.wav file
-#playsound("https:\\github.com\\astrocore-team\\Shrekked\\blob\\main\\MEME.mp3")
-
+playsound("https://github.com/astrocore-team/Shrekked/blob/main/MEME.mp3")
 
 #def on_closing():
 #    player = tkvideo("C:/Users/Jake Gorham/Documents/GitHub/Shrekked/MLG SHREK COMPILATION!.mp4", my_label, loop = 1, size = (1280,720))
