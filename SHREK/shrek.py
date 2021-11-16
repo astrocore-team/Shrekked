24a3e9baa2fe521671a4cc284d02e32c495008ac
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-16 21:17:37 +0000

    Update shrek.py

--- SHREK/shrek.py
+++ SHREK/shrek.py
@@ -52,10 +52,10 @@ threading.Thread(target=play).start()
 player = tkvideo("C:/SHREK/MLG SHREK COMPILATION!.mp4", my_label, loop = 1, size = (1280,720))
 player.play()
 
-#def on_closing():
-#    player = tkvideo("C:/SHREK/MLG SHREK COMPILATION!.mp4", my_label, loop = 1, size = (1280,720))
-#    player.play()
+def on_closing():
+   player = tkvideo("C:/SHREK/MLG SHREK COMPILATION!.mp4", my_label, loop = 1, size = (1280,720))
+   player.play()
 
-#root.protocol("WM_DELETE_WINDOW", on_closing)
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
