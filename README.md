e0113dcb83d27d0a91b2e724388d3b66a6b3223d
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2021-11-22 12:16:09 +0000

    dw

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Shrekked
-a unclosable virus that sticks a video on your screen and locks it blasting a funny shrek video on full volume.
+an unclosable virus that sticks a video on your screen and locks it blasting a funny shrek video on full volume with moaning sound.
---
+++ first_setup.sh
@@ -0,0 +1,15 @@
+$url = "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
+$output = "C:/tmp/python-3.9.7-amd64.exe"
+
+if (Test-Path $output) {
+    Write-Host "Script exists - skipping installation"
+    return;
+}
+
+New-Item -ItemType Directory -Force -Path C:/tmp
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+& $output /passive InstallAllUsers=1 PrependPath=1 Include_test=0
